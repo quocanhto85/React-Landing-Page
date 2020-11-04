a8812c0a6c0dd6ad8cc38823cb49012f3c8fdf1c
anhtpq <quocanhto85@gmail.com>
2020-11-04 17:29:53 +0700

    write navbar css, create showButton function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,24 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import Button from "./Button";
+import "./Navbar.css";
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click);
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false);
+        } else {
+            setButton(true)
+        }
+    }
+
+    window.addEventListener("resize", showButton);
 
     return (
         <>
